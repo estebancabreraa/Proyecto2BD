5775a82db0b57353fd89581955a41d0e5b1d468a
Alejandro-PC\Alejandro <mon17014@uvg.edu.gt>
2019-05-15 22:24:19 -0600

    Create EjemploConexion.py

---
+++ EjemploConexion.py
@@ -0,0 +1,45 @@
+import psycopg2
+from psycopg2 import Error
+"""
+Conexion en computadora de Raul Monzon:
+        user = "postgres",
+        password = "raul1998",
+        host = "127.0.0.1",
+        port = "5433",
+        database = "postgres"
+        
+Conexion en computadora de Esteban Cabrera: 
+        user = "postgres",
+        password = "       ",
+        host = "127.0.0.1",
+        port = "5432",
+        database = "postgres"
+        
+"""
+try:
+    
+    connection = psycopg2.connect(user = "postgres",
+                                  password = "raul1998",
+                                  host = "127.0.0.1",
+                                  port = "5433",
+                                  database = "postgres")
+    cursor = connection.cursor()
+    
+    create_table_query = '''CREATE TABLE mobile
+          (ID INT PRIMARY KEY     NOT NULL,
+          MODEL           TEXT    NOT NULL,
+          PRICE         REAL); '''
+    
+    cursor.execute(create_table_query)
+    
+    connection.commit()
+    
+    print("Table created successfully in PostgreSQL ")
+except (Exception, psycopg2.DatabaseError) as error :
+    print ("Error while creating PostgreSQL table", error)
+finally:
+    #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
